2088fa822cc60e2616b0f7761096bb0a6f8a9b1d
VasilyGorbunov <vasily.gorbunov@gmail.com>
2020-06-17 20:59:14 +0300

    25

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,39 @@
-module.exports.getByCategoryId = (req, res) => {
+const Position = require('../models/Position')
+const errorHandler = require('../utils/errorHandler')
 
+module.exports.getByCategoryId = async (req, res) => {
+  try {
+    const positions = await Position.find({
+      category: req.params.categoryId,
+      user: req.user.id
+    })
+
+    res.status(200).json(positions)
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
 
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+  try {
 
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
 
-module.exports.remove = (req, res) => {
+module.exports.remove = async (req, res) => {
+  try {
 
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
 
-module.exports.update = (req, res) => {
+module.exports.update = async (req, res) => {
+  try {
 
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
